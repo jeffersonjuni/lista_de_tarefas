97a90671cf4a0e3c244169ce64c1d23507a2ae94
jeffersonjuni <jeftsantis76@gmail.com>
2025-05-26 21:17:19 -0300

    update checkbox

--- src/components/Tarefas/index.js
+++ src/components/Tarefas/index.js
@@ -1,33 +1,26 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { FaEdit, FaWindowClose } from 'react-icons/fa'
+import { FaEdit, FaTrash } from "react-icons/fa";
+import './Tarefas.css';
 
-import './Tarefas.css'
-
-export default function Tarefas({tarefas, handleEdit, handleDelete}) {
+export default function Tarefas({ tarefas, concluidas, handleEdit, handleDelete, handleToggleConcluida }) {
   return (
     <ul className="tarefas">
       {tarefas.map((tarefa, index) => (
-        <li key={tarefa}>
-          {tarefa}
+        <li key={index} className={concluidas[index] ? "concluida" : ""}>
+          <label>
+            <input
+              type="checkbox"
+              checked={concluidas[index]}
+              onChange={() => handleToggleConcluida(index)}
+            />
+            <span style={{ marginLeft: "8px" }}>{tarefa}</span>
+          </label>
           <span>
-            <FaEdit
-              className="edit"
-              onClick={(e) => handleEdit(e, index)}
-            />
-            <FaWindowClose
-              className="delete"
-              onClick={(e) => handleDelete(e, index)}
-            />
+            <FaEdit className="edit" onClick={(e) => handleEdit(e, index)} />
+            <FaTrash className="delete" onClick={(e) => handleDelete(e, index)} />
           </span>
         </li>
       ))}
     </ul>
   );
 }
-
-Tarefas.propTypes = {
-  tarefas: PropTypes.array.isRequired,
-  handleEdit: PropTypes.func.isRequired,
-  handleDelete: PropTypes.func.isRequired,
-};
--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 import Form from "./Form";
 import Tarefas from "./Tarefas";
 
@@ -10,6 +9,7 @@ export default class Main extends Component {
   state = {
     novaTarefa: "",
     tarefas: [],
+    concluidas: [],
     index: -1,
     mensagemErro: "",
     erroVisivel: false,
@@ -19,70 +19,64 @@ export default class Main extends Component {
 
   componentDidMount() {
     const tarefas = JSON.parse(localStorage.getItem("tarefas"));
+    const concluidas = JSON.parse(localStorage.getItem("concluidas"));
 
-    if (!tarefas) return;
-    this.setState({ tarefas });
+    if (tarefas) {
+      this.setState({
+        tarefas,
+        concluidas: concluidas || Array(tarefas.length).fill(false),
+      });
+    }
   }
 
   componentDidUpdate(prevProps, prevState) {
-    const { tarefas } = this.state;
-
-    if (tarefas === prevState.tarefas) return;
-
-    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+    const { tarefas, concluidas } = this.state;
+    if (tarefas !== prevState.tarefas)
+      localStorage.setItem("tarefas", JSON.stringify(tarefas));
+    if (concluidas !== prevState.concluidas)
+      localStorage.setItem("concluidas", JSON.stringify(concluidas));
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const { tarefas, index } = this.state;
+    const { tarefas, concluidas, index } = this.state;
     let { novaTarefa } = this.state;
     novaTarefa = novaTarefa.trim();
 
     if (novaTarefa === "") {
-      this.mostrarErro(
-        "Por favor, preencha o campo antes de adicionar a tarefa."
-      );
+      this.mostrarErro("Por favor, preencha o campo antes de adicionar a tarefa.");
       return;
     }
 
-    if (tarefas.indexOf(novaTarefa) !== -1) {
+    if (tarefas.indexOf(novaTarefa) !== -1 && index === -1) {
       this.mostrarErro("Essa tarefa já existe!");
       return;
     }
 
     const novasTarefas = [...tarefas];
+    const novasConcluidas = [...concluidas];
 
     if (index === -1) {
+      novasTarefas.push(novaTarefa);
+      novasConcluidas.push(false);
+
       this.setState({
-        tarefas: [...novasTarefas, novaTarefa],
+        tarefas: novasTarefas,
+        concluidas: novasConcluidas,
         novaTarefa: "",
+        mensagemErro: "",
       });
       this.mostrarSucesso("Tarefa adicionada com sucesso!");
     } else {
       novasTarefas[index] = novaTarefa;
 
       this.setState({
-        tarefas: [...novasTarefas],
+        tarefas: novasTarefas,
         index: -1,
         novaTarefa: "",
+        mensagemErro: "",
       });
       this.mostrarSucesso("Tarefa editada com sucesso!");
-    }
-
-    if (index === -1) {
-      this.setState({
-        tarefas: [...novasTarefas, novaTarefa],
-        novaTarefa: "",
-        mensagemErro: "", // Limpa erro após sucesso
-      });
-    } else {
-      novasTarefas[index] = novaTarefa;
-      this.setState({
-        tarefas: [...novasTarefas],
-        index: -1,
-        novaTarefa: "",
-        mensagemErro: "", // Limpa erro após edição
-      });
     }
   };
 
@@ -101,16 +95,19 @@ export default class Main extends Component {
   };
 
   handleDelete = (e, index) => {
-    const { tarefas } = this.state;
-    const novasTarefas = [...tarefas];
-    novasTarefas.splice(index, 1);
-
-    this.setState({
-      tarefas: [...novasTarefas],
-    });
+    const tarefas = [...this.state.tarefas];
+    const concluidas = [...this.state.concluidas];
+    tarefas.splice(index, 1);
+    concluidas.splice(index, 1);
+    this.setState({ tarefas, concluidas });
   };
 
-  //mensagem de sucesso
+  handleToggleConcluida = (index) => {
+    const concluidas = [...this.state.concluidas];
+    concluidas[index] = !concluidas[index];
+    this.setState({ concluidas });
+  };
+
   mostrarSucesso = (mensagem) => {
     this.setState({ mensagemSucesso: mensagem, sucessoVisivel: true });
 
@@ -123,7 +120,6 @@ export default class Main extends Component {
     }, 3000);
   };
 
-  //mensagem de erro
   mostrarErro = (mensagem) => {
     this.setState({ mensagemErro: mensagem, erroVisivel: true });
 
@@ -132,23 +128,19 @@ export default class Main extends Component {
 
       setTimeout(() => {
         this.setState({ mensagemErro: "" });
-      }, 500); // espera o fade-out terminar
-    }, 3000); // visível por 3 segundos
+      }, 500);
+    }, 3000);
   };
 
   render() {
-    const { novaTarefa, tarefas } = this.state;
+    const { novaTarefa, tarefas, concluidas } = this.state;
 
     return (
       <div className="main">
         <h1>Lista de Tarefas</h1>
 
         {this.state.mensagemErro && (
-          <div
-            className={`alert-erro ${
-              !this.state.erroVisivel ? "fade-out" : ""
-            }`}
-          >
+          <div className={`alert-erro ${!this.state.erroVisivel ? "fade-out" : ""}`}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path d="M1 21h22L12 2 1 21zm12-3h-2v2h2v-2zm0-8h-2v6h2v-6z" />
             </svg>
@@ -157,11 +149,7 @@ export default class Main extends Component {
         )}
 
         {this.state.mensagemSucesso && (
-          <div
-            className={`alert-sucesso ${
-              !this.state.sucessoVisivel ? "fade-out" : ""
-            }`}
-          >
+          <div className={`alert-sucesso ${!this.state.sucessoVisivel ? "fade-out" : ""}`}>
             {this.state.mensagemSucesso}
           </div>
         )}
@@ -174,8 +162,10 @@ export default class Main extends Component {
 
         <Tarefas
           tarefas={tarefas}
+          concluidas={concluidas}
           handleEdit={this.handleEdit}
           handleDelete={this.handleDelete}
+          handleToggleConcluida={this.handleToggleConcluida}
         />
       </div>
     );
